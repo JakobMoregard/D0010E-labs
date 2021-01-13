8e1b68dd75951fbe96e9239f065e43f6c35c7a72
Jakob Moregård <74310723+JakobMoregard@users.noreply.github.com>
2021-01-13 15:35:02 +0100

    Add files via upload

---
+++ Lab2/src/lab2/Main.java
@@ -0,0 +1,22 @@
+package lab2;
+
+/**
+ * @author Mattias Nilsson and Jakob Moregård
+ * 
+ * 
+ * Programet skappar en spelbarna där en spelare kan gå mellan rum.
+ * 
+ */
+
+import lab2.Driver;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Driver driver = new Driver();
+		
+		driver.run();
+		
+	}
+}
